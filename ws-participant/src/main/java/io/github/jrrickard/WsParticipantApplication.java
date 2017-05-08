817633d0e5103e9dbe6a3cb3088500bc968583c8
Jeremy Rickard <jrickard@vmware.com>
2017-05-08 16:00:27 -0600

    Initial Commit

---
+++ ws-participant/src/main/java/io/github/jrrickard/WsParticipantApplication.java
@@ -0,0 +1,12 @@
+package io.github.jrrickard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsParticipantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsParticipantApplication.class, args);
+	}
+}

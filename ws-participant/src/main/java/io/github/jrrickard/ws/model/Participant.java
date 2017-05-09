40c7e5fa8e5a8e9d78b4325428596f65df1cb7f3
Jeremy Rickard <jrickard@vmware.com>
2017-05-08 21:02:19 -0600

    Polish, Lombok

--- ws-participant/src/main/java/io/github/jrrickard/ws/model/Participant.java
+++ ws-participant/src/main/java/io/github/jrrickard/ws/model/Participant.java
@@ -1,11 +1,13 @@
 package io.github.jrrickard.ws.model;
 
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.Date;
 
 @Document
+@Data
 public class Participant {
 
     enum Gender {
@@ -21,60 +23,4 @@ public class Participant {
     private Gender gender;
     private String city;
     private String state;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(final String id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(final String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(final String lastName) {
-        this.lastName = lastName;
-    }
-
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(final Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public Gender getGender() {
-        return gender;
-    }
-
-    public void setGender(final Gender gender) {
-        this.gender = gender;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(final String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(final String state) {
-        this.state = state;
-    }
 }
